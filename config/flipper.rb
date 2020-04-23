45abb4aaec473ce05ff25d780b5c26a8e9a6de9f
Danilo Barion Nogueira <danilo.barion@gmail.com>
2020-04-23 17:53:45 -0300

    Add comments

--- config/flipper.rb
+++ config/flipper.rb
@@ -12,7 +12,12 @@ Flipper.configure do |config|
   end
 end
 
+# It allows managing flags visually
+# https://github.com/jnunemaker/flipper/tree/master/docs/ui
 FLIPPER_UI_APP = Flipper::UI.app(Flipper) { |builder|
   builder.use Rack::Session::Cookie, secret: "something long and random"
 }
+
+# It allows managing flags with API requests
+# https://github.com/jnunemaker/flipper/blob/master/docs/api
 FLIPPER_API_APP = Flipper::Api.app(Flipper)
